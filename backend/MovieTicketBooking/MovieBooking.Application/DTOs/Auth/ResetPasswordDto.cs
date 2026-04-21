5aa6a7d4935fe15f3eba31178c214e770e5e4c13
sachinthacham <sachinthachamindu26@gmail.com>
2026-04-21 20:31:24 +0530

    implement reset password dto

---
+++ backend/MovieTicketBooking/MovieBooking.Application/DTOs/Auth/ResetPasswordDto.cs
@@ -0,0 +1,8 @@
+namespace MovieBooking.Application.DTOs.Auth;
+
+public class ResetPasswordDto
+{
+    public string Email { get; set; } = default!;
+    public string Token { get; set; } = default!;
+    public string NewPassword { get; set; } = default!;
+}

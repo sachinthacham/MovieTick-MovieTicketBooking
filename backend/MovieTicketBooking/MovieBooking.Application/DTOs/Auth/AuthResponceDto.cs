102cad9a2070291e088fab760e6ab985993d68c8
sachinthacham <sachinthachamindu26@gmail.com>
2026-04-21 20:11:22 +0530

    implement auth response

---
+++ backend/MovieTicketBooking/MovieBooking.Application/DTOs/Auth/AuthResponceDto.cs
@@ -0,0 +1,7 @@
+﻿namespace MovieBooking.Application.DTOs.Auth;
+
+public class AuthResponseDto
+{
+    public string AccessToken { get; set; } = default!;
+    public string RefreshToken { get; set; } = default!;
+}

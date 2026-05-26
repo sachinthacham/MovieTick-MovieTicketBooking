7839f438d4ade66d428f347a8932445c94592ad4
sachinthacham <sachinthachamindu26@gmail.com>
2026-05-26 20:54:37 +0530

    update seat category entity

--- backend/MovieTicketBooking/MovieBooking.Domain/Entities/SeatCategory.cs
+++ backend/MovieTicketBooking/MovieBooking.Domain/Entities/SeatCategory.cs
@@ -6,9 +6,11 @@ public class SeatCategory : BaseEntity
 {
     public string Name { get; set; } = default!;
     public decimal DefaultPrice { get; set; }
-    public string Color { get; set; } = "#000000";
     public string? Description { get; set; }
+    public string Color { get; set; } = "#000000";
+   
 
-    public ICollection<Seat> Seats { get; set; } = [];
     public ICollection<ShowtimePricing> ShowtimePricings { get; set; } = [];
+    public ICollection<Seat> Seats { get; set; } = [];
+    
 }

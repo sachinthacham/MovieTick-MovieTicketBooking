75a65d8763c0069e5802b150b7ad598ed9dd8ed5
sachinthacham <sachinthachamindu26@gmail.com>
2026-05-26 20:52:59 +0530

    update moview review interface

--- backend/MovieTicketBooking/MovieBooking.Application/Interfaces/IMovieReviewRepository.cs
+++ backend/MovieTicketBooking/MovieBooking.Application/Interfaces/IMovieReviewRepository.cs
@@ -4,9 +4,11 @@ namespace MovieBooking.Application.Interfaces;
 
 public interface IMovieReviewRepository
 {
-    Task AddAsync(MovieReview review);
-    Task<MovieReview?> GetByIdAsync(Guid id);
+
     Task<List<MovieReview>> GetByMovieAsync(Guid movieId, bool approvedOnly = true);
     Task UpdateAsync(MovieReview review);
+    Task<MovieReview?> GetByIdAsync(Guid id);
+
     Task DeleteAsync(MovieReview review);
+     Task AddAsync(MovieReview review);
 }
